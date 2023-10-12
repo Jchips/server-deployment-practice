87ebc454681cb67a7ae11cf570a682a7ad3d1725
Jchips <20979096+Jchips@users.noreply.github.com>
2023-10-12 12:08:10 -0700

    added tests, fixed error

--- handlers/500.js
+++ handlers/500.js
@@ -3,7 +3,8 @@
 module.exports = (error, req, res, next) => {
   res.status(500).send({
     error: 500,
-    route: req.query,
+    route: req.path,
+    query: req.query,
     body: req.body,
     message: `Server Error: ${error}`,
   });
